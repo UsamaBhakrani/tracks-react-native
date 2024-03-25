75946f9a6e27e5b8ff9ace3ab76357c557ad8902
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-25 17:06:46 +0500

    location permissions via expo

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import MapView, { Polyline } from "react-native-maps";
 const Map = () => {
   let points = [];
--- src/screens/TrackCreateScreen.jsx
+++ src/screens/TrackCreateScreen.jsx
@@ -2,14 +2,33 @@ import { StyleSheet, View } from "react-native";
 import { Text } from "@rneui/themed";
 import Map from "../components/Map";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { requestForegroundPermissionsAsync } from "expo-location";
+import { useEffect, useState } from "react";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
   const insets = useSafeAreaInsets();
+
+  const startWatching = async () => {
+    try {
+      await requestForegroundPermissionsAsync();
+    } catch (error) {
+      setErr(error);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
 
   return (
     <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
       <Text h2>Create a Track</Text>
       <Map />
+      {err && (
+        <Text style={{ color: "red" }}>please enable location services</Text>
+      )}
     </View>
   );
 };
